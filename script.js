7a810add678be06e147f88e7a4a5a4b95699d5ee
greatgreater <taneetai16@gmail.com>
2022-04-14 16:03:01 +0800

    second prototype

--- script.js
+++ script.js
@@ -15,71 +15,107 @@ let computerPlay = () => {
 }
 
 let playRound = (playerSelection, computerSelection ) => {
-    let draw = () => alert("Draw!");
-    let lost = () => alert("You lost!");
-    let won = () => alert("You won!");
+    let draw = () => "Draw!";
+    let lost = () => "You lost!";
+    let won = () => "You won!";
 
     //playerchoice = rock
     if (playerSelection === "rock" && computerSelection === "rock")
     {
-        draw();
+        return "draw";
     }
 
     else if (playerSelection === "rock" && computerSelection === "paper")
     {
-        lost();
+        return "lost";
     }
 
     else if (playerSelection === "rock" && computerSelection === "scissors")
     {
-        won();
+        return "won";
     }
     //playerchoice = paper
     if (playerSelection === "paper" && computerSelection === "rock")
     {
-        won();
+        return "won";
     }
 
     else if (playerSelection === "paper" && computerSelection === "paper")
     {
-        draw();
+        return "draw";
     }
 
     else if (playerSelection === "paper" && computerSelection === "scissors")
     {
-        lost();
+        return "lost";
     }
     //playerchoice = scissors
     if (playerSelection === "scissors" && computerSelection === "rock")
     {
-        lost();
+        return "lost";
     }
 
     else if (playerSelection === "scissors" && computerSelection === "paper")
     {
-        won();
+        return "won";
     }
 
     else if (playerSelection === "scissors" && computerSelection === "scissors")
     {
-        draw();
+        return "draw";
     }
 
 }
 
 let game = (rounds) => {
+    let playerScore = 0;
+    let computerScore = 0;
+    let gameScore;
+
     for (let i = 1; i <= rounds; i++)
     {
-        let playerSelection = ''
-        //takes player input as case insensitive
-        playerSelection = prompt("What is your choice?").toLowerCase();
-        console.log(playerSelection); 
+        let playerSelection = '';
+        let validSelection = 0;
+        
+        do 
+        {
+            playerSelection = prompt("What is your choice?").toLowerCase();
+            if (playerSelection === "rock" || playerSelection === "paper"
+            || playerSelection === "scissors")
+                validSelection = 1;
+            else
+                alert("Invalid choice! Try again.")
+        }
+        while (validSelection === 0)
 
-        // while (playerSelection !== "rock" || playerSelection !== "paper" || playerSelection !== "scissors");
         let computerSelection = computerPlay();
-        playRound(playerSelection, computerSelection);
+        let outcome = playRound(playerSelection, computerSelection);
+        if (outcome === "won")
+        {
+            alert("You won!");
+            playerScore++;
+        }
+        else if (outcome === "lost")
+        {
+            alert("You lost!");
+            computerScore++;
+        }
+        else 
+        {
+            alert("Draw!");
+        }
+
+        gameScore = 'Player: ' + playerScore + ' | Computer: ' + computerScore;
+        console.log(gameScore); 
     }
+    if (playerScore > computerScore)
+        alert("You won!");
+    else if (playerScore < computerScore)
+        alert("You lost!");
+    else 
+        alert("It's a tie!");
 }
 
 const rounds = 5;
+
 game(rounds);
